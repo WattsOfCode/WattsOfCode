6e3679e3f4a77d394571ea5b3973273ee47902da
John Deardorff <66809813+WattsOfCode@users.noreply.github.com>
2025-08-11 15:15:59 -0400

    Simple python files
    terminal & turtle

---
+++ python/Week1.1/main.py
@@ -0,0 +1,17 @@
+'''
+Name : John Deardorff
+Recreation Date : 08/06/2025
+
+Description:
+    Applicatoion(program) calling functions with imports
+
+    This application purpose is to inform you of some simple 
+    information about your computer such as manufacutre, model,
+    name, pcu, system type, and family.
+'''
+
+import SystemInfo    
+
+print("John Deardorff's system notice")
+SystemInfo.funSystemInfo()
+
